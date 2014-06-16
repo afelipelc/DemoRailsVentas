aa81d41a5c11b2803413c1c643e1b96c3cfe25fb
AFelipe <afelipelc@gmail.com>
2014-06-15 23:59:29 -0500

    Busqueda implementada en Productos

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,11 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    if params[:q]
+      @products = Product.where("nombre  LIKE '%"+params[:q]+"%'")
+    else
+      @products = Product.all
+    end
   end
 
   # GET /products/1
